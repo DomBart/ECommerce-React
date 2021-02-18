4583ffc73e6d9bcdf56bda6d70736ea4e2f6f541
DomBart <domantas.bartusevicius@gmail.com>
2021-02-18 20:33:07 +0200

    stripe checkout

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -9,17 +9,16 @@ import { commerce } from '../../../lib/commerce';
 
 const steps = ['Shipping address', 'Payment details'];
 
-const Checkout = ({ cart }) => {
+const Checkout = ({ cart, order, onCaptureCheckout, error }) => {
     const classes = useStyles();
     const [activeStep, setActiveStep] = useState(0);
-    const [checkoutToken, setCheckoutToken] = useState(null)
+    const [checkoutToken, setCheckoutToken] = useState(null);
+    const [shippingData, setShippingData] = useState({});
 
     useEffect(() => {
         const generateToken = async () => {
             try{
                 const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' });
-
-                console.log(token);
 
                 setCheckoutToken(token);
             }
@@ -31,6 +30,19 @@ const Checkout = ({ cart }) => {
         generateToken();
     }, [cart])
 
+    const nextStep = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    }
+
+    const backStep = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    }
+
+    const next = (data) => {
+        setShippingData(data);
+        nextStep();
+    }
+
     const Confirmation = () => (
         <div>
             Confirmation
@@ -38,8 +50,8 @@ const Checkout = ({ cart }) => {
     )
 
     const Form = () => activeStep === 0
-    ? <AddressForm checkoutToken={checkoutToken} />
-    : <PaymentForm />
+    ? <AddressForm checkoutToken={checkoutToken} next={next} />
+    : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} nextStep={nextStep} backStep={backStep} onCaptureCheckout={onCaptureCheckout} />
     return (
         <>
           <div className={classes.toolbar} />
