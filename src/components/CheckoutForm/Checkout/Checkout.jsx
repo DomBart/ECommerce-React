be7c6501b5e74470bcd524fa939dbd3ae2e81dc3
DomBart <domantas.bartusevicius@gmail.com>
2021-02-19 15:08:11 +0200

    push to home after successful checkout

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button, CssBaseline} from '@material-ui/core';
 import useStyles from './styles';
 import AddressForm from '../AddressForm';
@@ -12,6 +12,8 @@ const steps = ['Shipping address', 'Payment details'];
 
 const Checkout = ({ cart, order, onCaptureCheckout, error }) => {
     const classes = useStyles();
+    const history = useHistory();
+
     const [activeStep, setActiveStep] = useState(0);
     const [checkoutToken, setCheckoutToken] = useState(null);
     const [shippingData, setShippingData] = useState({});
@@ -24,7 +26,7 @@ const Checkout = ({ cart, order, onCaptureCheckout, error }) => {
                 setCheckoutToken(token);
             }
             catch (error) {
-                console.log(error);
+                history.pushState('/');
             }
         }
 
